df92552714e484a06bb26f6d2635a84fbfc5d193
judeallanhill <judeallanhill@icloud.com>
2024-02-01 20:18:27 -0800

    Fixing Origin blocks

--- Server/main.js
+++ Server/main.js
@@ -10,11 +10,14 @@ const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3500;
-let newUser = false;
-const clientId = '3f9756c0e365406b866a';
-const clientSecret = '2657f125505c135d73786f452d7095edefd71ad4'
 let currentOnlineUsers = ["NoName"];
-app.use(cors({ origin: [ "*"] }));
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
